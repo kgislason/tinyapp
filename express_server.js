8632af277ec7106c1a2e42b39c76005e13894fdd
Kristy Gislason <kristygislason@gmail.com>
2022-10-24 11:51:13 -0700

    Added Nodemon: a utility that will monitor for any changes in our source code and automatically restart our server.

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const { generateRandomString } = require("./functions");
 
 app.set('view engine', 'ejs');
 
-// BOdy parser - needs to come before routes
+// Body parser - needs to come before routes
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
@@ -13,42 +13,48 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.render("pages/index", {
-    urlDatabase
-  });
-});
 
-app.get("/urls", (req, res) => {
+// Home Page
+app.get("/", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("pages/urls_index", templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
+// Create New hort url form page
+app.get('/u/new', (req, res) => {
   res.render("pages/urls_new");
 });
 
-app.get("/urls.json", (req, res) => {
+// Database
+app.get("/u.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.post("/urls/", (req, res) => {
+app.post("/", (req, res) => {
   //generate new short URL id
   const random = generateRandomString(6);
   // Add it to database:
   urlDatabase[random] = req.body.longURL;
-  
-  // Redirect to newly created short url
-  res.redirect('/urls/' + random);
+  // res.redirect('/' + random);
+  res.redirect('/');
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get("/u/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
-  res.render("pages/urls_show", templateVars);
+  res.redirect(templateVars.longURL);
 });
+
+// Edge Cases
+// What would happen if a client requests a short URL with a non-existant id?
+// What happens to the urlDatabase when the server is restarted?
+//What type of status code do our redirects have? What does this status code mean?
+// Warning
+/* 
+In order to function correctly, any long URLs submitted through the form must include the protocol (http:// or https://). In a real web app, we might implement some code to help our users by detecting whether http was included in their submission, and add it if it wasn't. Because we want to focus more on the routing, request handling, and templates for this project, we're going to omit this feature. However, it means that, while testing, you may encounter problems if the sample URLs you are submitting do not include the protocol.
+*/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
