5d73e6423b452a72ce1c016bda65bfb4e0f765eb
Kristy Gislason <kristygislason@gmail.com>
2022-11-07 09:47:53 -0800

    Use quotes consistently in database object

--- database.js
+++ database.js
@@ -28,12 +28,12 @@ const urlDatabase = {
 };
 
 const users = {
-  userRandomID: {
+  "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
     password: "purple-monkey-dinosaur",
   },
-  user2RandomID: {
+  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "dishwasher-funk",
